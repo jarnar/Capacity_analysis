e74bac03da9510493a87c579905029e3a88e51ab
jarnar <jarnar@qq.com>
2018-07-13 16:29:07 +0800

    add bootsrap

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@
 /*
  * GET home page.
  */
+ 
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.sendfile("./views/home.html"); 
+  // res.sendfile("./views/index.html"); 
+  // res.sendfile("./views/calendar-pie.html"); 
 };
+
